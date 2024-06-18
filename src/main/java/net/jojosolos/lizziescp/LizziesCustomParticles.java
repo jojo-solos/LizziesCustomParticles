66fa9ccc88fad60a21ddb98e942f73f3ed1da165
jojosolos <beatsolos.b@gmail.com>
2024-06-18 18:06:56 -0500

    Set up

---
+++ src/main/java/net/jojosolos/lizziescp/LizziesCustomParticles.java
@@ -0,0 +1,16 @@
+package net.jojosolos.lizziescp;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LizziesCustomParticles implements ModInitializer {
+	public static final String MODID = "lizziescp";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/jojosolos/lizziescp/LizziesCustomParticlesClient.java
@@ -0,0 +1,10 @@
+package net.jojosolos.lizziescp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LizziesCustomParticlesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
